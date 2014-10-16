a58df53157aaad0bb8dc9a7420fa4a22cf19aec4
Thuan Duc Nguyen <vpenguiin@gmail.com>
2014-10-16 18:48:31 +0200

    Renamed Assignments and GroupPractices

---
+++ Assignments/AOP4.c
@@ -0,0 +1,76 @@
+/* Afleveringsopgave 4 - solveQuadraticEquation
+   Skrevet af Thuan Duc Nguyen                  */
+   
+#include <stdio.h>
+#include <math.h>
+
+double calc_disc(double a, double b, double c){
+  /* Define local variables */
+  double discriminant;
+  
+  /* Calculate the discriminant */
+  discriminant = b * b - 4 * a * c;
+  
+  return discriminant;
+}
+
+double calc_1root(double a, double b){
+  /* Define local variables */
+  double root1;
+  
+  /* Calculate the only root */
+  root1 = -b/(2*a);
+  
+  return root1;
+}
+
+double calc_2root1(double a, double b, double c){
+  /* Define local variables */
+  double root1,discriminant;
+  
+  /* Calculate discriminant */
+  discriminant = calc_disc(a,b,c);
+  
+  /* Calculate the two roots */
+  root1 = (-b + sqrt(discriminant))/(2*a);
+  
+  return root1;
+}
+
+double calc_2root2(double a, double b, double c){
+ /* Define local variables */
+  double root2,discriminant;
+  
+  /* Calculate discriminant */
+  discriminant = calc_disc(a,b,c);
+  
+  /* Calculate the two roots */
+  root2 = (-b - sqrt(discriminant))/(2*a);
+  
+  return root2;
+}
+
+int main(void) {
+  /* Define variables */
+  double a,b,c;
+
+  /* Prompt input */
+  printf("\nEnter the values a, b and c: ");
+  scanf("%lf %lf %lf",&a,&b,&c);
+  
+  /* Print the discriminant */
+  printf("\nThe discriminant is: %.2f\n\n",calc_disc(a,b,c));
+  
+  /* Print Solutions */
+  if(calc_disc(a,b,c) < 0)
+    printf("  No solution:\n  No root\n\n");
+  else if(calc_disc(a,b,c) == 0)
+    printf("  One solution:\n  Root: %.2f\n\n",
+           calc_1root(a,b));
+  else
+    printf("  Two solutions:\n  First root: %.2f\n  Second root: %.2f\n\n",
+           calc_2root1(a,b,c),
+           calc_2root2(a,b,c));
+
+  return 0;
+}
